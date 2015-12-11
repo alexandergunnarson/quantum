c361912be0d05631b7154dbc103a8de2f009d347
Alex Gunnarson <alexandergunnarson@gmail.com>
2015-12-11 13:01:11 -0700

    Added better support for Google Places API

--- src/clj/quantum/apis/google/places/core.clj
+++ src/clj/quantum/apis/google/places/core.clj
@@ -4,20 +4,134 @@
 (def search-url "https://maps.googleapis.com/maps/api/place/nearbysearch/json")
 
 (defrecord GeoCoordinate [lat long])
+; (GeoCoordinate. 0 0 #_lat #_long)
 
-(defn search
+(def valid-place-types
+  #{"accounting"
+    "airport"
+    "amusement_park"
+    "aquarium"
+    "art_gallery"
+    "atm"
+    "bakery"
+    "bank"
+    "bar"
+    "beauty_salon"
+    "bicycle_store"
+    "book_store"
+    "bowling_alley"
+    "bus_station"
+    "cafe"
+    "campground"
+    "car_dealer"
+    "car_rental"
+    "car_repair"
+    "car_wash"
+    "casino"
+    "cemetery"
+    "church"
+    "city_hall"
+    "clothing_store"
+    "convenience_store"
+    "courthouse"
+    "dentist"
+    "department_store"
+    "doctor"
+    "electrician"
+    "electronics_store"
+    "embassy"
+    "establishment"
+    "finance"
+    "fire_station"
+    "florist"
+    "food"
+    "funeral_home"
+    "furniture_store"
+    "gas_station"
+    "general_contractor"
+    "grocery_or_supermarket"
+    "gym"
+    "hair_care"
+    "hardware_store"
+    "health"
+    "hindu_temple"
+    "home_goods_store"
+    "hospital"
+    "insurance_agency"
+    "jewelry_store"
+    "laundry"
+    "lawyer"
+    "library"
+    "liquor_store"
+    "local_government_office"
+    "locksmith"
+    "lodging"
+    "meal_delivery"
+    "meal_takeaway"
+    "mosque"
+    "movie_rental"
+    "movie_theater"
+    "moving_company"
+    "museum"
+    "night_club"
+    "painter"
+    "park"
+    "parking"
+    "pet_store"
+    "pharmacy"
+    "physiotherapist"
+    "place_of_worship"
+    "plumber"
+    "police"
+    "post_office"
+    "real_estate_agency"
+    "restaurant"
+    "roofing_contractor"
+    "rv_park"
+    "school"
+    "shoe_store"
+    "shopping_mall"
+    "spa"
+    "stadium"
+    "storage"
+    "store"
+    "subway_station"
+    "synagogue"
+    "taxi_stand"
+    "train_station"
+    "travel_agency"
+    "university"
+    "veterinary_care"
+    "zoo"})
+
+(defn+ search
   "Called 'Nearby Search / Place Search'"
-  {:info-url "https://developers.google.com/places/webservice/search"}
-  [{:keys [radius]
-    :or {radius 50000}}]
-  (with-throw (and (<= radius 50000) (> radius 0))
-    (Err. :out-of-range "Radius out of range:" radius))
+  {:usage '(search {:long -111.90948486 :lat 40.56180797}
+             (auth/datum :google "fake@gmail.com" :api-key))
+   :info "https://developers.google.com/places/webservice/search"}
+  [coord api-key & [{:keys [radius search-type place-types parse?]
+                       :or {radius 50000}}]]
+  (assert (nnil? api-key))
+  (assert (in? search-type #{:fuzzy :exact}))
+  (assert (and (<= radius 50000) (> radius 0)) #{radius})
 
-  (http/request! search-url
-    {"key" (:client-id (auth/auth-keys :google))
-     "location" (str/join "," ((juxt :lat :long) (GeoCoordinate. 0 0 #_lat #_long)))
-     ; Defines the distance (in meters) within which to return place results. 
-     "radius" radius}) )
+  (let [location (str (:lat coord) "," (:long coord))
+        search-opts (condp = search-type
+                      :fuzzy {"radius" radius}
+                      :exact {"rankby" "distance"
+                              "types"  (if place-types
+                                           (str/join "|" place-types)
+                                           (extern (str/join "|" valid-place-types)))})]
+   
+    (http/request!
+      {:url search-url
+       :parse? parse?
+       :query-params
+         (mergel search-opts
+           {"key"      api-key
+            "location" location
+            ; Defines the distance (in meters) within which to return place results. 
+            })})))
 
 
 ;  types=food&name=cruise&key=API_KEY
